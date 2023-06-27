1281e86d6aa148f38cb9f7465a2811bcc57b7eb1
hawkf07 <fikrfifadilah299@gmail.com>
2023-06-27 20:16:59 +0700

    work so far

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+
 const typography = require("@tailwindcss/typography");
+const colors = require('tailwindcss/colors')
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+		colors:{
+			primary:colors.green[400]
+		}	
+		},
 	},
 	plugins: [typography()],
 };

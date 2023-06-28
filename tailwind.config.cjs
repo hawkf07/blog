575f81a804a30dbb49a55ac69138253da910a5d0
hawkf07 <fikrfifadilah299@gmail.com>
2023-06-28 08:47:46 +0700

    fix:color scheme is not right

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,19 @@
-
 const typography = require("@tailwindcss/typography");
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
-		colors:{
-			primary:colors.blue[400]
-		}	,fontFamily:{
-			roboto:['Roboto',"sans-serif"]
-			,poppins:['Poppins','sans-serif']
-		}
+			colors: {
+				primary: colors.blue[400],
+			},
+			fontFamily: {
+				roboto: ["Roboto", "sans-serif"],
+				poppins: ["Poppins", "sans-serif"],
+			},
 		},
 	},
+	darkMode: "class",
 	plugins: [typography()],
 };

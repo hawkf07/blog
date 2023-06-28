1c1fef226816854abdff1fb0aa9f38955ab7e80a
hawkf07 <fikrfifadilah299@gmail.com>
2023-06-28 08:53:10 +0700

    fix:color scheme is not right-2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ const typography = require("@tailwindcss/typography");
 const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	important: true,
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
